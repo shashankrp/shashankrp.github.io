40b9d5862d33eeca83d75efe28f1b7bc9c16a359
shashankrp <30649674+shashankrp@users.noreply.github.com>
2021-03-28 16:03:50 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,6 +52,7 @@ sr.reveal('.skills__img',{delay: 600});
 
 /*SCROLL WORK*/
 sr.reveal('.work__img',{interval: 200}); 
+sr.reveal('.work__img1',{interval: 200});
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
